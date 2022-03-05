1ebe24dc48ffea27a126e1d094e23cadee76ac77
Matty Lacey <69597356+mattyblackout@users.noreply.github.com>
2022-03-05 03:49:31 +0000

    edit welcomescreen.py

--- welcomescreen.py
+++ welcomescreen.py
@@ -1,10 +1,11 @@
 import simpleguitk as simplegui
 
 def draw(canvas):
-    canvas.draw_text("Welcome to Osmos!", [280, 300], 50, "Red")
+    canvas.draw_text("Welcome to CS1821 Osmos!", [150, 200], 60, "White")
+    canvas.draw_text("Press any key to start a new game", [220, 400], 40, "Red")
 
 # Create a frame and assign callbacks to event handlers
-frame = simplegui.create_frame("Home", 960, 540)
+frame = simplegui.create_frame("Home", 1024, 576)
 frame.set_draw_handler(draw)
 
 # Start the frame animation
