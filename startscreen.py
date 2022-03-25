856ec31c4aac826064727726a624621a850de664
Matty Lacey <69597356+mattyblackout@users.noreply.github.com>
2022-03-25 07:08:53 +0000

    edit startscreen.py

--- startscreen.py
+++ startscreen.py
@@ -1,21 +1,20 @@
 import simpleguitk as simplegui
 
-BACKGROUNDIMG = simplegui.load_image('https://images2.imgbox.com/4a/38/b798aeH2_o.jpg')
+BACKGROUND_IMAGE = simplegui.load_image('https://images2.imgbox.com/4a/38/b798aeH2_o.jpg')
 
-LOGOIMG = simplegui.load_image('https://images2.imgbox.com/1d/dd/4Kj7pUOr_o.png')
-LOGOIMG_DIMS = (1007, 235)
+LOGO_IMAGE = simplegui.load_image('https://images2.imgbox.com/1d/dd/4Kj7pUOr_o.png')
+LOGO_IMAGE_DIMS = (1007, 235)
 
-TEXTIMG = simplegui.load_image('https://images2.imgbox.com/4f/bc/cLxcNfmt_o.png')
-TEXTIMG_DIMS = (1139, 178)
-
+PRESSKEY_IMAGE = simplegui.load_image('https://images2.imgbox.com/4f/bc/cLxcNfmt_o.png')
+PRESSKEY_IMAGE_DIMS = (1139, 178)
 
 def draw(canvas):
-    canvas.draw_image(BACKGROUNDIMG, (0, 0), (2650,1600), [10, 10], (2650,1600))
-    canvas.draw_image(LOGOIMG, (503, 117), LOGOIMG_DIMS, (650, 300), (1007, 235), 0)
-    canvas.draw_image(TEXTIMG, (570, 89), TEXTIMG_DIMS, (650, 450), (1139, 178), 0)
+    canvas.draw_image(BACKGROUND_IMAGE, (0, 0), (2650,1600), [10, 10], (2650,1600))
+    canvas.draw_image(LOGO_IMAGE, (503, 117), LOGO_IMAGE_DIMS, (650, 300), (1007, 235), 0)
+    canvas.draw_image(PRESSKEY_IMAGE, (570, 89), PRESSKEY_IMAGE_DIMS, (650, 450), (1139, 178), 0)
 
 # Create a frame and assign callbacks to event handlers
-frame = simplegui.create_frame("Home", 1315, 790)
+frame = simplegui.create_frame("Main Menu", 1315, 790)
 frame.set_draw_handler(draw)
 
 # Start the frame animation
