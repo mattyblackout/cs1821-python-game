f4a9ba9a6b1ec450dee1422d78d2e633e914bfc7
Matty Lacey <69597356+mattyblackout@users.noreply.github.com>
2022-03-25 06:16:54 +0000

    edit startscreen.py

--- startscreen.py
+++ startscreen.py
@@ -10,7 +10,7 @@ TEXTIMG_DIMS = (1139, 178)
 
 
 def draw(canvas):
-    canvas.draw_image(BACKGROUNDIMG, (10, 10), (2650,1600), [10, 10], (2650,1600))
+    canvas.draw_image(BACKGROUNDIMG, (0, 0), (2650,1600), [10, 10], (2650,1600))
     canvas.draw_image(LOGOIMG, (503, 117), LOGOIMG_DIMS, (650, 300), (1007, 235), 0)
     canvas.draw_image(TEXTIMG, (570, 89), TEXTIMG_DIMS, (650, 450), (1139, 178), 0)
 
